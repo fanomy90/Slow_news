6bc6cb16d8e98ccea8b16d625b5417db0d163851
skiner <fanomy90@gmail.com>
2024-07-28 13:32:47 +0300

    продолжаю эксперименты над стилизацией сайта

--- puppeteer/news/static/news/js/script.js
+++ puppeteer/news/static/news/js/script.js
@@ -4,3 +4,27 @@ $(document).ready(function() {
         $('body').toggleClass('lock');
     });
 });
+// скрипт для замены тега br на p на странице post.html
+document.addEventListener('DOMContentLoaded', function() {
+    let contentDiv = document.querySelector('.news-content');
+    if (contentDiv) {
+        let paragraphs = contentDiv.innerHTML.split('<br>').join('</p><p>');
+        paragraphs = '<p>' + paragraphs + '</p>';
+        contentDiv.innerHTML = paragraphs;
+    }
+});
+// скрипт для замены тега br на p на странице index.html
+document.addEventListener('DOMContentLoaded', function() {
+    let contentDivs = document.querySelectorAll('.news-content');
+    contentDivs.forEach(function(contentDiv) {
+        // Получаем HTML содержимое
+        let html = contentDiv.innerHTML;
+        // Заменяем <br> на </p><p>, добавляем <p> в начале и конце
+        let newHtml = html.split('<br>').join('</p><p>');
+        if (newHtml) {
+            newHtml = '<p>' + newHtml + '</p>';
+        }
+        // Устанавливаем новое HTML содержимое
+        contentDiv.innerHTML = newHtml;
+    });
+});
